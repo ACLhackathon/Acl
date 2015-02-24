f56f0e3d94d395ddb64d8a86a4106a22f53820cd
Jesse <jesse.xu.20@gmail.com>
2015-02-23 19:54:16 -0500

    create & add rules

--- src/acl/Acl.java
+++ src/acl/Acl.java
@@ -1,23 +1,45 @@
 package acl;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 
 public class Acl {
+	private static Acl instanceAcl;
 	private Hashtable<String, List<AclEntity>> hashtable;
 	private boolean access;
-	public Acl() {
+	public static Acl getInstance(){
+		if (instanceAcl==null) {
+			return new Acl();
+		}
+		else return instanceAcl;
+	}
+	private Acl() {
 		// TODO Auto-generated constructor stub
 	}
 	
 	public boolean Acl_list_create(String name,String action){
-		return false;
+		if (hashtable.containsKey(name)) {
+			return false;
+		}
+		else {
+			List<AclEntity> list=new ArrayList<AclEntity>();
+			hashtable.put(name, list);
+			return true;
+		}
 	}
 	
 	public boolean Acl_add_rule(String ACLNAME,String SRC_IP_PREFIX, String DST_IP_PREFIX, 
 			String PROTO,String SRC_PORT,String DST_PORT,String PRIORITY,String ACTION){
-		
-		return false;
+		if (!hashtable.contains(ACLNAME)) {
+			return false;
+		}
+		else{
+			List<AclEntity> list=hashtable.get(ACLNAME);
+			AclEntity aclEntity=new AclEntity(ACLNAME,SRC_IP_PREFIX, DST_IP_PREFIX, PROTO, SRC_PORT, DST_PORT, PRIORITY,ACTION);
+			list.add(aclEntity);
+			return true;
+		}
 	}
 	public void Acl_check_packet(String ACLNAME, String SRC_IP,String DST_IP,String PROTO,String SRC_PORT,String DST_PORT){
 		
